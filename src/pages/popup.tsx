75a82d8c37afa59b6956d329c80cc2864f314bee
Vitor Falcao <vitorfhc@protonmail.com>
2024-09-20 12:12:44 +0000

    Refactor package dependencies to remove Bootstrap and Bootstrap Icons

--- src/pages/popup.tsx
+++ src/pages/popup.tsx
@@ -1,19 +1,9 @@
-import React, { ReactElement, ReactNode } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Popup = () => {
-  const handleClick = () => {
-    chrome.runtime.sendMessage({ greeting: 'hello' }, (response) => {
-      console.log('Response:', response);
-    });
-  };
-
   return (
-    <div>
-      <h1>Popup</h1>
-      <button type="button" className="btn btn-primary" onClick={handleClick}>Click me</button>
-    </div>
+    <div></div>
   );
 };
 
