a37674fb8d2ab3ee72d3e2b725af7ca9945954e6
Vitor Falcao <vitorfhc@protonmail.com>
2024-09-20 19:49:20 +0000

    Creates Popup and Toggle

---
+++ src/components/ui/toggle.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import React, { useState } from 'react'
+import { Description, Field, Label, Switch } from '@headlessui/react'
+
+interface ToggleProps {
+    label: string;
+    enabled: boolean;
+    setEnabled: (enabled: boolean) => void;
+}
+
+export default function Toggle({ label, enabled, setEnabled }: ToggleProps) {
+
+    return (
+        <Field className="flex items-center justify-between">
+            <span className="flex flex-grow flex-col">
+                <Label as="span" passive className="text-sm font-medium leading-6 text-gray-900">
+                    {label}
+                </Label>
+            </span>
+            <Switch
+                checked={enabled}
+                onChange={setEnabled}
+                className="group relative inline-flex h-5 w-10 flex-shrink-0 cursor-pointer items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2"
+            >
+                <span className="sr-only">Use setting</span>
+                <span aria-hidden="true" className="pointer-events-none absolute h-full w-full rounded-md bg-white" />
+                <span
+                    aria-hidden="true"
+                    className="pointer-events-none absolute mx-auto h-4 w-9 rounded-full bg-gray-200 transition-colors duration-200 ease-in-out group-data-[checked]:bg-black"
+                />
+                <span
+                    aria-hidden="true"
+                    className="pointer-events-none absolute left-0 inline-block h-5 w-5 transform rounded-full border border-gray-200 bg-white shadow ring-0 transition-transform duration-200 ease-in-out group-data-[checked]:translate-x-5"
+                />
+            </Switch>
+        </Field>
+    )
+}
--- src/pages/popup.tsx
+++ src/pages/popup.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import Toggle from '../components/ui/toggle';
+import '../tailwind/styles.css';
 
-const Popup = () => {
+export default function Popup() {
   return (
-    <div></div>
-  );
-};
+    <div className="min-w-[300px] p-4 bg-white">
+      <p className="text-xl font-semibold mb-4">Settings</p>
+      <hr className="mb-4"/>
+      <div className="space-y-4">
+        <Toggle label="Search in Query Values" enabled={true} setEnabled={() => { }} />
+        <Toggle label="Search in Path" enabled={true} setEnabled={() => { }} />
+        <Toggle label="Search for null/undefined" enabled={true} setEnabled={() => { }} />
+      </div>
+      <hr className="my-4"/>
+      <div className="space-y-4">
+        <Toggle label="Partial Matching" enabled={true} setEnabled={() => { }} />
+        <Toggle label="Clear Findings on Refresh" enabled={true} setEnabled={() => { }} />
+      </div>
+      <hr className="my-4"/>
+      <button
+        type="button"
+        className="min-w-full rounded-md bg-slate-800 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-slate-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-600"
+      >
+        See Findings
+      </button>
+    </div>
+  )
+}
 
 const container = document.getElementById('root');
 if (container) {
