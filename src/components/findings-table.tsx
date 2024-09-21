7e94232ebfb592ffaace8f75e30c1b5f62f7ff4b
Vitor Falcao <vitorfhc@protonmail.com>
2024-09-21 14:33:54 +0000

    Fixes devtools panel buttons styling

--- src/components/findings-table.tsx
+++ src/components/findings-table.tsx
@@ -56,7 +56,7 @@ export default function FindingsTable({ onRowClick }: FindingsTableProps) {
         <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
           <button
             type="button"
-            className="min-w-full rounded-md bg-slate-800 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-slate-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-600"
+            className="max-w-40 min-w-40 rounded-md bg-slate-800 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-slate-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-600"
           >
             Settings
           </button>
@@ -64,7 +64,7 @@ export default function FindingsTable({ onRowClick }: FindingsTableProps) {
         <div className="mt-2 sm:ml-2 sm:mt-0 sm:flex-none">
           <button
             type="button"
-            className="block rounded-md bg-danger px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-danger-dark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="max-w-40 min-w-40 block rounded-md bg-danger px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-danger-dark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-danger-dark"
             onClick={clearFindings}
           >
             Clear findings
