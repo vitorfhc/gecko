f45985a043ab96cc19af8681b6a3c87dc20acf22
Vitor Falcao <vitorfhc@protonmail.com>
2024-09-22 13:26:01 +0000

    Removes findings table header

--- src/components/findings-table.tsx
+++ src/components/findings-table.tsx
@@ -69,18 +69,6 @@ export default function FindingsTable({ onRowClick }: FindingsTableProps) {
           <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
             <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
               <table className="min-w-full divide-y divide-gray-300 cursor-pointer table-fixed">
-                <thead className="bg-gray-50">
-                  <tr>
-                    <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
-                      #
-                    </th>
-                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                      Request
-                    </th>
-                    <th scope="col" className="px-3 py-3.5">
-                    </th>
-                  </tr>
-                </thead>
                 <tbody className="divide-y divide-gray-200 bg-white">
                   {findings.map((finding) => (
                     <tr key={finding.index} className="hover:bg-gray-50" onClick={() => onRowClick(finding)}>
